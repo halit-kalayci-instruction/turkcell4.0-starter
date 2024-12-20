58972b96a87e7303f0a26df8fe884e8a96b3858f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-12-20 18:56:29 +0300

    değişkenler

--- src/main/java/com/turkcell/Main.java
+++ src/main/java/com/turkcell/Main.java
@@ -1,10 +1,31 @@
 package com.turkcell;
 
 public class Main {
-  public static void main(String[] args) {
+  // Main class scope'u
+  public static void main(String[] args)
+  {
     System.out.println("Hello world!");
-
     System.out.println("Merhaba, Turkcell");
     // Bu bir yorum satırıdır.
+
+    // Scope - Kapsam (Bağlam)
+    // Java -> tip güvenli
+    int number = 0;
+    int number1 = 1; // null
+    System.out.println(number);
+    System.out.println(number1);
+
+    String name = "Adem"; // string name = "";
+
+    {
+      // c scope
+      System.out.println(number);
+      int cScopeNumber = 50;
+    }
+
+    // System.out.println(cScopeNumber); // Compile-time error
+
+    // List,Array
   }
 }
+//a->b->c
