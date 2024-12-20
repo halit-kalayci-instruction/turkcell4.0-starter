7b6c655c0e72d42c8ef6151c3bbb8e7a0408896d
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-12-20 19:06:20 +0300

    koleksiyonlar

--- src/main/java/com/turkcell/Main.java
+++ src/main/java/com/turkcell/Main.java
@@ -1,5 +1,8 @@
 package com.turkcell;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   // Main class scope'u
   public static void main(String[] args)
@@ -25,7 +28,27 @@ public class Main {
 
     // System.out.println(cScopeNumber); // Compile-time error
 
-    // List,Array
+    // Array,List
+    String[] names = new String[5];
+    names[0] = "Adem";
+    names[1] = "Şevval";
+    names[2] = "Merve";
+    names[3] = "Çınar";
+    names[4] = "Ramazan";
+
+    int[] ages = { 18,20,21,25,30,35 };
+
+    System.out.println(ages[3]);
+    System.out.println(names[2]);
+    //System.out.println(names);
+
+    // Liste yapıları
+    // interfaceler => newlenemez
+    List<String> nameList = new ArrayList<>();
+    nameList.add("Duygu");
+    nameList.add("Rabia");
+
+    System.out.println(nameList);
   }
 }
 //a->b->c
